04f74f6a5dccfdbc0da74d9210eb7ef6bab16029
saikishore30 <kishoresai005@gmail.com>
2023-12-20 20:40:18 +0530

    updates

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,80 +1,45 @@
-<<<<<<< HEAD
-const taskie = require("../todo");
+const tuduList = require("../todo");
 
-describe("todoList Test Suite", () => {
-  let tudu;
+describe("TodoList Test Suite", () => {
+  let ted;
 
   beforeEach(() => {
-    tudu = taskie();
+    ted = tuduList();
   });
 
   test("We Should create a new todo", () => {
-    tudu.add({
-=======
-const taskmanager = require("../todo");
-
-describe("todoList Test Suite", () => {
-  let td;
-
-  beforeEach(() => {
-    td = taskmanager();
-  });
-
-  test("We Should create a new todo", () => {
-    td.add({
->>>>>>> a0ca2d06f609613df0020d1a6b7cee0f5b0086e3
+    ted.add({
       title: "New Todo",
       completed: false,
       dueDate: "2023-12-31",
     });
 
-<<<<<<< HEAD
-    expect(tudu.all.length).toBe(1);
-    expect(tudu.all[0].title).toBe("New Todo");
+    expect(ted.all.length).toBe(1);
+    expect(ted.all[0].title).toBe("New Todo");
   });
 
   test("Should mark a todo as completed after its completion", () => {
-    tudu.add({
-=======
-    expect(td.all.length).toBe(1);
-    expect(td.all[0].title).toBe("New Todo");
-  });
-
-  test("Should mark a todo as completed after its completion", () => {
-    td.add({
->>>>>>> a0ca2d06f609613df0020d1a6b7cee0f5b0086e3
+    ted.add({
       title: "Incomplete Todo",
       completed: false,
       dueDate: "2023-12-31",
     });
 
-<<<<<<< HEAD
-    tudu.markAsComplete(0);
+    ted.markAsComplete(0);
 
-    expect(tudu.all[0].completed).toBe(true);
+    expect(ted.all[0].completed).toBe(true);
   });
 
   test("Should retrieve overdue items after the time is over", () => {
-    tudu.add({
-=======
-    td.markAsComplete(0);
+    
 
-    expect(td.all[0].completed).toBe(true);
-  });
-
-  test("Should retrieve overdue items after the time is over", () => {
-    td.add({
->>>>>>> a0ca2d06f609613df0020d1a6b7cee0f5b0086e3
+    ted.add({
       title: "Overdue Todo",
       completed: false,
       dueDate: "2023-01-01",
     });
 
-<<<<<<< HEAD
-    const allOverDueThings = tudu.overdue();
-=======
-    const allOverDueThings = td.overdue();
->>>>>>> a0ca2d06f609613df0020d1a6b7cee0f5b0086e3
+    const allOverDueThings = ted.overdue();
 
     expect(allOverDueThings.length).toBe(1);
     expect(allOverDueThings[0].title).toBe("Overdue Todo");
@@ -83,42 +48,28 @@ describe("todoList Test Suite", () => {
   test("Should retrieve due today items which are mentioned", () => {
     const todayDate = new Date().toISOString().split("T")[0];
 
-<<<<<<< HEAD
-    tudu.add({
-=======
-    td.add({
->>>>>>> a0ca2d06f609613df0020d1a6b7cee0f5b0086e3
+    ted.add({
       title: "Due Today Todo",
       completed: false,
       dueDate: todayDate,
     });
 
-<<<<<<< HEAD
-    const todayDueThings = tudu.dueToday();
-=======
-    const todayDueThings = td.dueToday();
->>>>>>> a0ca2d06f609613df0020d1a6b7cee0f5b0086e3
+    const todayDueThings = ted.dueToday();
 
     expect(todayDueThings.length).toBe(1);
     expect(todayDueThings[0].title).toBe("Due Today Todo");
   });
 
   test("Should retrieve due later items", () => {
-<<<<<<< HEAD
-    tudu.add({
-=======
-    td.add({
->>>>>>> a0ca2d06f609613df0020d1a6b7cee0f5b0086e3
+    
+
+    ted.add({
       title: "Due Later Todo",
       completed: false,
       dueDate: "2023-12-31",
     });
 
-<<<<<<< HEAD
-    const dueLaterThings = tudu.dueLater();
-=======
-    const dueLaterThings = td.dueLater();
->>>>>>> a0ca2d06f609613df0020d1a6b7cee0f5b0086e3
+    const dueLaterThings = ted.dueLater();
 
     expect(dueLaterThings.length).toBe(1);
     expect(dueLaterThings[0].title).toBe("Due Later Todo");
